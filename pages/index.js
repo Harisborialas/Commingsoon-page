389571b7592c8d12e80fb2a7935a8844c941c50f
@Harisborialas <azan6743@gmail.com>
2023-03-13 15:39:26 +0500

    i made some changes

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,7 @@ const initvalues = {
 };
 const initState = {
   values: initvalues,
+  errorMsg: "",
 };
 export default function Home() {
   const [state, setState] = useState(initState);
@@ -16,12 +17,15 @@ export default function Home() {
   const onSubmit = async () => {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!emailRegex.test(values.email)) {
-      alert("Please enter a valid email address.");
+      errorMsg: "Please enter a valid email address.";
+      // alert("Please enter a valid email address.");
       return;
     }
     setState((prev) => ({
       ...prev,
+      errorMsg: "Please enter a valid email address.",
       isloading: true,
+      errorMsg: "",
     }));
     try {
       await sendContactForm(values);
@@ -44,45 +48,47 @@ export default function Home() {
       },
     }));
   };
-  
-  
-  const darktheme = () =>{
+
+  const darktheme = () => {
     document.body.classList.remove("light");
     document.body.classList.add("dark");
-  }
-  const lighttheme = () =>{
+  };
+  const lighttheme = () => {
     document.body.classList.remove("dark");
     document.body.classList.add("light");
-  }
-
-
- 
+  };
 
   return (
     <>
       <Head>
         <title>Create Next App</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;800&display=swap"
+          rel="stylesheet"></link>
       </Head>
       <div className="container-fluid" id="mainpage">
         <nav class="navbar" id="Navcol2">
           <div class="container">
-            <a class="navbar-brand" href="#">
+            <a class="navbar-brand d-flex" href="#">
               <img
                 src="/logo.png"
                 alt="Logo"
-                width="80"
-                height="80"
+                width="50"
+                height="50"
                 class="d-inline-block align-text-top"
               />
+              <h5 id="BYC">BYC</h5>
             </a>
             <div className="col-6 d-flex justify-content-end">
               <div class="dropdown">
                 <button class="dropbtn">Theme</button>
                 <div class="dropdown-content">
-                  <a href="#" id="dark-theme-link" onClick={darktheme} >
+                  <a href="#" id="dark-theme-link" onClick={darktheme}>
                     dark
                   </a>
-                  <a href="#" id="light-theme-link" onClick={lighttheme} >
+                  <a href="#" id="light-theme-link" onClick={lighttheme}>
                     light
                   </a>
                 </div>
@@ -92,14 +98,14 @@ export default function Home() {
         </nav>
 
         <div className="row" id="row2">
-          <div className="col-lg-6 col-md-5 col-sm-12" id="col2">
+          <div className="col-lg-6 col-md-6 col-sm-12" id="col2">
             <div className="row" id="backgroundimg">
               <div className="col-lg-12 col-md-12 col-sm-12">
-                <h1 className="text-white">
+                <h1 id="headingmain">
                   Something big is on
                   <br /> The horizon!
                 </h1>
-                <p className="text-white">Join us for the next big thing</p>
+                <p id="para">Join us for the next big thing</p>
               </div>
 
               <div className="col-lg-12 col-md-12 col-sm-12 " id="col3">
@@ -115,6 +121,11 @@ export default function Home() {
                     required
                     pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
                   />
+                  <div>
+                    {state.errorMsg && (
+                      <p style={{ color: "red" }}>{state.errorMsg}</p>
+                    )}
+                  </div>
                   <button
                     type="submit"
                     disabled={!values.email}
@@ -146,13 +157,18 @@ export default function Home() {
                 alt=""
                 width={100}
                 height={100}
+                id="twitter"
                 className="mt-5 mx-5"
               />
             </a>
           </div>
           <div className="col-lg-6 d-flex justify-content-end float-right">
-            <p className="mt-5 text-sm  mx-2 text-white">All right reserved</p>
-            <p className="mt-5 text-sm  mx-2 text-white">Privacy Policy</p>
+            <p className="mt-5 text-sm  mx-2" id="allrightrese">
+              All right reserved
+            </p>
+            <p className="mt-5 text-sm  mx-2" id="privacy">
+              Privacy Policy
+            </p>
           </div>
         </footer>
       </div>
