22d29c52a45b7543ef2e3a9f189a9708f5fb9e34
@Harisborialas <azan6743@gmail.com>
2023-03-18 17:07:18 +0500

    i add some changes to the dark mode and light mode in themes

--- Components/developerPage/commingSoonPageDev.js
+++ Components/developerPage/commingSoonPageDev.js
@@ -130,8 +130,8 @@ const CommingSoonPageDev = () => {
               </div>
             </div>
           </div>
-
-          <div className="row my-5">
+          {/* __________________________Dark Mode_______________________________________ */}
+          <div className="row my-5" id="middlerow1">
             <div className="col-lg-12 col-12">
               <div className="row">
                 <div
@@ -147,7 +147,7 @@ const CommingSoonPageDev = () => {
 
                 <div
                   className="col-lg-6 col-12 py-2 order-1 order-lg-2"
-                  id="midimgbackground">
+                  id="midimgbackground1">
                   <div className="row" id="rowheading">
                     <div className="col-12 pt-5">
                       <h1 className=" font-bold " id="heading">
@@ -198,6 +198,75 @@ const CommingSoonPageDev = () => {
               </div>
             </div>
           </div>
+          {/* ________________________________________For Light Mode__________________________________________ */}
+          <div className="row my-5 " id="middlerow2">
+            <div className="col-lg-12 col-12">
+              <div className="row">
+                <div
+                  className="col-lg-6 col-12 order-2 order-lg-1"
+                  id="imgmain1">
+                  <img src="/Dsideimage.svg" alt="" width={600} height={500} />
+                </div>
+                <div
+                  className="col-lg-6 col-12 order-2 order-lg-1"
+                  id="imgmain12">
+                  <img src="/Lsideimage.svg" alt="" width={600} height={500} />
+                </div>
+
+                <div
+                  className="col-lg-6 col-12 py-2 order-1 order-lg-2"
+                  id="midimgbackground2">
+                  <div className="row" id="rowheading">
+                    <div className="col-12 pt-5">
+                      <h1 className=" font-bold " id="heading">
+                        Something big is on the horizon!
+                      </h1>
+                      <p className="text-2xl font-bold " id="paragraph">
+                        Join us for the next big thing
+                      </p>
+                    </div>
+                  </div>
+                  <div className="row" id="imgmain2">
+                    <div className="col-12" id="imgcolmain2">
+                      <img src="/Dsideimage.svg" alt="" />
+                    </div>
+                  </div>
+                  <div className="row" id="imgmain22">
+                    <div className="col-12" id="imgcolmain2">
+                      <img src="/Lsideimage.svg" alt="" />
+                    </div>
+                  </div>
+                  <div className="row" id="rowform">
+                    <div className="col-12 pt-3 px-3" id="colform">
+                      <form>
+                        <input
+                          type={"email"}
+                          className="form-control my-2"
+                          id="email"
+                          placeholder="Email Address"
+                          name="email"
+                          value={values.email}
+                          onChange={handleChange}
+                          required
+                          pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
+                        />
+
+                        <button
+                          type="submit"
+                          onClick={onSubmit}
+                          isloading={isloading}
+                          className="btn mb-3 "
+                          id="btn">
+                          Get Notified
+                        </button>
+                      </form>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* ________________________________________END_____________________________________________________ */}
           <div className="row my-5" id="threedivrow">
             <div className="col-lg-4 col-12 p-5" id="threedivcol1">
               <img src="/tool.png" alt="" width={52} height={52} />
